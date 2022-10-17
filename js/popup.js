20ec8ed7f59eafa31e924911b2afcdcb576e5d44
ekaterina <ekivanova72@gmail.com>
2022-10-17 21:11:52 +0500

    Добавляет скрытие пустых блоков в карточке

--- js/popup.js
+++ js/popup.js
@@ -25,41 +25,62 @@ advertsList.forEach(({ offer, author }) => {
   };
 
   const getCapacity = () => {
+    const guestsNumber = `${offer.guests} ${offer.guests === 1 ? 'гостя' : 'гостей'}`;
     switch (true) {
       case offer.rooms < 2:
-        return `${offer.rooms} комната для ${offer.guests} гостей`;
+        return `${offer.rooms} комната для ${guestsNumber}`;
       case offer.rooms < 5:
-        return `${offer.rooms} комнаты для ${offer.guests} гостей`;
+        return `${offer.rooms} комнаты для ${guestsNumber}`;
       default:
-        return `${offer.rooms} комнат для ${offer.guests} гостей`;
+        return `${offer.rooms} комнат для ${guestsNumber}`;
     }
   };
 
   const getFeatures = () => {
     const featureItems = offer.features;
+    const featureListContainer = advertElement.querySelector('.popup__features');
     const featureList = advertElement.querySelectorAll('.popup__feature');
-    const modifiers = featureItems.map((featureItem) => `popup__feature--${featureItem}`);
+    if (featureItems.length) {
+      const modifiers = featureItems.map((featureItem) => `popup__feature--${featureItem}`);
 
-    featureList.forEach((feature) => {
-      const modifier = feature.classList[1];
+      featureList.forEach((feature) => {
+        const modifier = feature.classList[1];
 
-      if (!modifiers.includes(modifier)) {
-        feature.remove();
-      }
-    });
+        if (!modifiers.includes(modifier)) {
+          feature.remove();
+        }
+      });
+    } else {
+      featureListContainer.style.display = 'none';
+    }
+  };
 
+  const getDescription = () => {
+    const description = offer.description;
+    if (description) {
+      return description;
+    } else {
+      advertElement.querySelector('.popup__description').style.display = 'none';
+    }
   };
 
   const getPhotos = () => {
     const photosContainer = advertElement.querySelector('.popup__photos');
     const photoItem = photosContainer.querySelector('.popup__photo');
     const photoList = offer.photos;
+    photosContainer.innerHTML = '';
 
-    photoList.forEach((photo) => {
-      const newPhoto = photoItem.cloneNode(true);
-      newPhoto.src = photo;
-      photosContainer.append(newPhoto);
-    });
+    if (photoList.length) {
+      photoList.forEach((photo) => {
+        const newPhoto = photoItem.cloneNode(true);
+        newPhoto.src = photo;
+        photosContainer.append(newPhoto);
+      });
+
+    } else {
+      photosContainer.style.display = 'none';
+    }
+
   };
 
 
@@ -70,11 +91,11 @@ advertsList.forEach(({ offer, author }) => {
   advertElement.querySelector('.popup__text--capacity').textContent = getCapacity();
   advertElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
   advertElement.querySelector('.popup__features').content = getFeatures();
-  advertElement.querySelector('.popup__description').textContent = offer.description;
+  advertElement.querySelector('.popup__description').textContent = getDescription();
   advertElement.querySelector('.popup__photos').content = getPhotos();
   advertElement.querySelector('.popup__avatar').src = author.avatar;
   advertListFragment.append(advertElement);
   advertElementsList.push(advertElement);
 });
 
-mapCanvas.append(advertElementsList[9]);
+mapCanvas.append(advertElementsList[5]);
