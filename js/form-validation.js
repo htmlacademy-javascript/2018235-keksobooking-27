22e5c723e6da2e09a2e8005c50b10600e7249800
ekaterina <ekivanova72@gmail.com>
2022-10-27 08:36:24 +0500

    Валиация полей время заезда и выезда и типа жилья

--- js/form-validation.js
+++ js/form-validation.js
@@ -6,6 +6,8 @@ const pristine = new Pristine(advertForm, {
   errorTextParent: 'ad-form__element',
 });
 
+// валидация количества комнат и количества гостей
+
 const capacity = advertForm.querySelector('#capacity');
 const roomNumber = advertForm.querySelector('#room_number');
 const capacityOption = {
@@ -21,9 +23,60 @@ const getCapacityErrorMessage = () => 'количество гостей не м
 pristine.addValidator(capacity, validateCapacity, getCapacityErrorMessage);
 pristine.addValidator(roomNumber, validateCapacity, getCapacityErrorMessage);
 
+function onCapacityChange() {
+  pristine.validate(capacity);
+}
+
+function onRoomNumberChange() {
+  pristine.validate(roomNumber);
+}
+
+roomNumber.addEventListener('change', onCapacityChange);
+capacity.addEventListener('change', onRoomNumberChange);
+
+// валидация цены в зависимости от типа жилья
+
+const type = document.querySelector('#type');
+const price = document.querySelector('#price');
+const priceSettings = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+
+const validatePriceSetting = () => price.value >= priceSettings[type.value];
+const getPriceErrorMessage = () => `Цена не может быть меньше ${priceSettings[type.value]} для этого типа жилья`;
+
+pristine.addValidator(price, validatePriceSetting, getPriceErrorMessage);
+
+function onTypeChange() {
+  price.placeholder = priceSettings[type.value];
+  pristine.validate(price);
+}
+
+type.addEventListener('change', onTypeChange);
+
+// валидация времени заезда и выезда
+
+const timein = advertForm.querySelector('#timein');
+const timeout = advertForm.querySelector('#timeout');
+
+function onTimeinChange() {
+  timeout.value = timein.value;
+  pristine.validate(timeout);
+}
+
+function onTimeoutChange() {
+  timein.value = timeout.value;
+  pristine.validate(timein);
+}
+
+timein.addEventListener('change', onTimeinChange);
+timeout.addEventListener('change', onTimeoutChange);
+
 advertForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
 });
-
-
